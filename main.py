9d494faab104e507e06112a2064a5230528e1b9f
Ramazan Abakarov <abakarov-roma@mail.ru>
2023-08-12 16:06:15 +0500

    Реализовал получение данных через файл. Реализовал GUI и помещение данных в таблицу

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import asyncio
 import aiohttp
 import re
+import csv
 import gi
-import requests
 from gi.repository import Gtk
 
 gi.require_version("Gtk", "3.0")
@@ -11,31 +11,52 @@ gi.require_version("Gtk", "3.0")
 class MyWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="Hello World")
-        self.set_default_size(800, 600)
-        self.box = Gtk.Box()
-        self.add(self.box)
-        """self.grid = Gtk.Grid()
+        self.set_size_request(600, 800)
+        self.grid = Gtk.Grid()
         self.grid.set_column_homogeneous(True)
         self.grid.set_row_homogeneous(True)
-        self.add(self.grid)"""
+        self.add(self.grid)
 
-        self.goods_and_price = Gtk.ListStore(str, int)
+        self.goods_and_price = Gtk.ListStore(str, str)
 
+        # кнопки
+        self.buttons = list()
         self.button_API = Gtk.Button(label="Загрузить из API")
-        self.button_API.set_size_request(80, 80)
+        self.buttons.append(self.button_API)
         self.button_API.connect("clicked", self.run_requests_API)
-        self.box.pack_start(self.button_API, True, False, 0)
 
         self.button_file = Gtk.Button(label="Загрузить из файла")
-        self.button_file.connect("clicked", self.on_button_clicked_file)
-        self.box.pack_start(self.button_file, True, False, 0)
+        self.buttons.append(self.button_file)
+        self.button_file.connect("clicked", self.reading_with_file)
 
+        self.scrollable_treelist = Gtk.ScrolledWindow()
+        self.scrollable_treelist.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.EXTERNAL)
+        self.scrollable_treelist.set_vexpand(False)
+        self.grid.attach(self.buttons[0], 0, 5, 1, 1)
+        self.grid.attach(self.buttons[1], 1, 5, 1, 1)
+        # столбцы
+        self.treeview = Gtk.TreeView(model=self.goods_and_price)
+        self.renderer_text_goods = Gtk.CellRendererText()
+        self.column_text_goods = Gtk.TreeViewColumn("Goods", self.renderer_text_goods, text=0)
+        self.treeview.append_column(self.column_text_goods)
+
+        self.renderer_text_price = Gtk.CellRendererText()
+        self.column_text_price = Gtk.TreeViewColumn("Price", self.renderer_text_price, text=1)
+        self.treeview.append_column(self.column_text_price)
+
+        self.scrollable_treelist.add(self.treeview)
+        self.grid.attach(self.scrollable_treelist, 0, 0, 2, 5)
+
+    def reading_with_file(self, widget):
+        with open("test_base.csv", "r") as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                self.goods_and_price.append([row["Title"], row["Price"]])
 
     def run_requests_API(self, widget):
         asyncio.run(self.async_request_API())
 
-    @staticmethod
-    async def async_request_API():
+    async def async_request_API(self):
         API = ["https://paycon.su/api1.php", "https://paycon.su/api2.php"]
 
         async with aiohttp.ClientSession() as session:
@@ -48,12 +69,13 @@ class MyWindow(Gtk.Window):
         result_name = re.findall(r"name[^\r]+", result_response)
         result_price = re.findall(r"price[^\r]+", result_response)
 
-        list_dict_name_price = {i[0][8:-2].replace('\\', ''): i[1][8:] for i in zip(result_name, result_price)}
-        print(list_dict_name_price)
+        list_dict_name_price = [(i[0][8:-2].replace('\\', ''), i[1][8:]) for i in zip(result_name, result_price)]
+        for row in list_dict_name_price:
+            self.goods_and_price.append(row)
 
 
 if __name__ == "__main__":
     main_window = MyWindow()
-    main_window.connect("delete-event", Gtk.main_quit)
+    main_window.connect("destroy", Gtk.main_quit)
     main_window.show_all()
     Gtk.main()
